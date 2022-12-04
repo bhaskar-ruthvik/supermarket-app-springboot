ec0a9e719672e614448a82c3ebbd0bbbad643c7f
Your Name <pritam.nandikonda@gmail.com>
2022-12-04 12:44:12 +0530

    admin pgae added

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,12 +1,25 @@
+import Chart from 'components/Chart'
 import Header from 'components/Header'
-import TableAdmin from 'components/TableAdmin'
+import BasicTable from 'components/TableAdmin'
+import Widget from 'components/Widget'
 import React from 'react'
+import './admin.css'
 
 function Admin() {
   return (
-    <div>
+    <div className="app">
       <Header></Header>
-      <TableAdmin />
+      <div className="admin-table">
+      <BasicTable></BasicTable>
+      </div>
+      
+      <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+      {/* <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} /> */}
     </div>
   )
 }
